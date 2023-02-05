bd1c2df713fe2cb333fadcd9351fdcb3ce2d7255
Brian Rheingans <brheingans@dslextreme.com>
2023-02-04 22:25:47 -0800

    Working buildroot kernel module build, also with rootfs_overlay

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,13 +5,16 @@
 #
 ##############################################################
 
-LDD_VERSION = '7927bd384f44d57ce5dfa7c731b9fe5183de7c85'
+LDD_VERSION = 'af14e3a89f40e342a8824cb7b0b8f78b91ecac71'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-booglabri.git'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-booglabri.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
